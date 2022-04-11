1a84ce7040297865be0ab5f5fb15e88945a245fb
Akhil <adev93117@gmail.com>
2022-04-11 11:15:49 +0530

    version 1

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,11 +1,2 @@
-# include<stdio.h>
 
-int main(){
-    int length,breadth;
-    printf("enter length \n");
-    scanf ("%d",&length);
-    printf("enter breadth \n");
-    scanf ("%d",&breadth);
-    printf("area %d",length*breadth);
-    return 0;
-}
+int get_digits(int n){
